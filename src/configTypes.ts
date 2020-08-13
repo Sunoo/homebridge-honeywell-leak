bc6d9e98480ef6f43fe7de5eb524f66d8d7bb5b4
Donavan Becker <donavan.becker@icloud.com>
2020-08-13 10:02:33 -0500

    Fix issue with refresh token

--- src/configTypes.ts
+++
@@ -1,9 +0,0 @@
-export type HoneywellLeakPlatformConfig = {
-  name: string;
-  consumer_key: string;
-  consumer_secret: string;
-  refresh_token: string;
-  polling_minutes: number;
-  hide_temperature: boolean;
-  hide_humidity: boolean;
-};
