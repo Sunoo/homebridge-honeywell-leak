1e75f4254275097e1c8e9ab3de0c38639b0c3e06
Donavan Becker <donavanbecker@thebeckers.local>
2020-10-02 11:14:26 -0500

    Update platform.ts

--- src/platform.ts
+++ src/platform.ts
@@ -122,13 +122,11 @@ export class HoneywellLeakPlatform implements DynamicPlatformPlugin {
       this.config.options.leaksensor.hide_leak;
     }
 
+    this.log.error(JSON.stringify(this.config.options.ttl));
     if (this.config.options) {
-      this.config.options.ttl = this.config.options?.ttl || 300;
-    } else {
-      this.config.options!.ttl = 300;
+      this.config.options.ttl = this.config.options!.ttl || 300;
     }
-
-    //this.config.options!.ttl = this.config.options?.ttl || 300; // default 300 seconds
+    this.log.error(JSON.stringify(this.config.options.ttl));
 
     if (!this.config.credentials?.consumerSecret && this.config.options!.ttl! < 300) {
       this.log.debug('TTL must be set to 300 or higher unless you setup your own consumerSecret.');
